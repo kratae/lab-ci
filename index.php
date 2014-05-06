3dd481d73f66dd25e239c74255fe2630bdc2c093
“kratae” <kratae_kikkok@hotmail.com>
2014-05-06 14:06:52 +0700

    change commit with my name

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENT KRATAE
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
